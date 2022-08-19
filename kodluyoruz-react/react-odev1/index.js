6967d2d0b89ac311504292cbdb7d13925b6a3a31
yunusemrebyzt <69965388+yunusemrebyzt@users.noreply.github.com>
2022-08-20 00:02:11 +0300

    React Odev1

--- kodluyoruz-react/react-odev1/index.js
+++ kodluyoruz-react/react-odev1/index.js
@@ -1,13 +1,3 @@
-import fetch from "node-fetch";
-import axios from "axios";
+import getData from "./app.js";
 
-async function getData(user_id){
-    const {data:users} = await axios("https://jsonplaceholder.typicode.com/users/"+user_id);
-    const {data:posts} = await axios("https://jsonplaceholder.typicode.com/posts/"+user_id);
-
-    console.log("users: ",users);
-    console.log("posts: ",posts);
-};
-
-
-export default getData;
+getData(1);
